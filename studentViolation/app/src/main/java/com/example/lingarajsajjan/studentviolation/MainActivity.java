d081fd32f29c3a67a825c10fb1f73761fd42cddb
Lingaraj sajjan <lingarajsajjan@Lingarajs-MacBook-Pro.local>
2017-11-28 18:22:54 +0530

    created the security person page

--- studentViolation/app/src/main/java/com/example/lingarajsajjan/studentviolation/MainActivity.java
+++ studentViolation/app/src/main/java/com/example/lingarajsajjan/studentviolation/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,17 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final Context context=MainActivity.this;
         TextView textView=(TextView)findViewById(R.id.createAccoutTxt);
+        Button loginbutton=(Button)findViewById(R.id.loginBtn);
 
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent voilationPage=new Intent(context,SecurityPerson.class);
+                startActivity(voilationPage);
+            }
+        });
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Context context=MainActivity.this;
+
                // Class createAccout= new Class();
                // Toast.makeText(context,"next page",Toast.LENGTH_SHORT).show();
-                Intent createAccout=new Intent(context,CreatAccount.class);
-                startActivity(createAccout);
+                Intent createAccount=new Intent(context,CreatAccount.class);
+                startActivity(createAccount);
             }
         });
     }
---
+++ studentViolation/app/src/main/java/com/example/lingarajsajjan/studentviolation/SecurityPerson.java
@@ -0,0 +1,25 @@
+package com.example.lingarajsajjan.studentviolation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecurityPerson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security_person);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+}
